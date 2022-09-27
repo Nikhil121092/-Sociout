77d202f30e5cc740ed6e541db1f634ce4b2daccd
Mayank Gorakh <mayank.javarnd@gmail.com>
2022-09-27 17:57:35 -0400

    sidebar implemented

--- sociout/src/components/common/leftSidebar.js
+++ sociout/src/components/common/leftSidebar.js
@@ -1,11 +1,55 @@
-// import { Nav } from "react-bootstrap";
+import React from 'react';
+import './leftSidebar.css';
+import {
+  RssFeed,
+  PeopleOutlined,
+  MessageOutlined,
+  NotificationsNoneOutlined,
+  TravelExploreOutlined,
+  PermIdentityOutlined,
+  SettingsOutlined,
+  LogoutOutlined
+} from "@mui/icons-material";
 
-const LeftSideBar = (props) => {
+export default function Sidebar() {
   return (
-    <div className="bg-secondary text-white fullHeight border-end">
-      LeftSideBar
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <PeopleOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">My community</span>
+          </li>
+          <li className="sidebarListItem">
+            <MessageOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Messages</span>
+          </li>
+          <li className="sidebarListItem">
+            <NotificationsNoneOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Notification</span>
+          </li>
+          <li className="sidebarListItem">
+            <TravelExploreOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Explore</span>
+          </li>
+          <li className="sidebarListItem">
+            <PermIdentityOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Profile</span>
+          </li>
+          <li className="sidebarListItem">
+            <SettingsOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Settings</span>
+          </li>
+          <li className="sidebarListItem">
+            <LogoutOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Logout</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
-};
-
-export default LeftSideBar;
+}
