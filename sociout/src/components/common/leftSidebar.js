d3a432a24b8b305adb138d3521cf17d8fe130289
Mayank Gorakh <mayank.javarnd@gmail.com>
2022-09-28 15:12:22 -0400

    required changes done

--- sociout/src/components/common/leftSidebar.js
+++ sociout/src/components/common/leftSidebar.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './leftSidebar.css';
 import {
-  RssFeed,
-  PeopleOutlined,
-  MessageOutlined,
-  NotificationsNoneOutlined,
-  TravelExploreOutlined,
+  HomeOutlined,
   PermIdentityOutlined,
-  SettingsOutlined,
-  LogoutOutlined
 } from "@mui/icons-material";
 
 export default function Sidebar() {
@@ -16,40 +10,24 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebarWrapper">
         <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <RssFeed className="sidebarIcon" />
-            <span className="sidebarListItemText">Feed</span>
-          </li>
-          <li className="sidebarListItem">
-            <PeopleOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">My community</span>
-          </li>
-          <li className="sidebarListItem">
-            <MessageOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Messages</span>
-          </li>
-          <li className="sidebarListItem">
-            <NotificationsNoneOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Notification</span>
-          </li>
-          <li className="sidebarListItem">
-            <TravelExploreOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Explore</span>
-          </li>
-          <li className="sidebarListItem">
-            <PermIdentityOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Profile</span>
-          </li>
-          <li className="sidebarListItem">
-            <SettingsOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Settings</span>
-          </li>
-          <li className="sidebarListItem">
-            <LogoutOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Logout</span>
-          </li>
+          <a href='/' className='anchor'>
+            <li className="sidebarListItem">
+              <HomeOutlined className="sidebarIcon" />
+              <span className="sidebarListItemText">Home</span>
+            </li>
+          </a>
+          <a href='/profile' className='anchor'>
+            <li className="sidebarListItem">
+              <PermIdentityOutlined className="sidebarIcon" />
+              <span className="sidebarListItemText">Profile </span>
+            </li>
+          </a>
         </ul>
       </div>
     </div>
   );
 }
+
+
+
+
