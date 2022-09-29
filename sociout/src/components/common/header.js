ab79451e95470391dec22e7f74b6ac21903e21d7
Vaibhav Vishap <vaibhavvishap.javaventures@gmail.com>
2022-09-29 16:08:49 -0400

    header implemented

--- sociout/src/components/common/header.js
+++ sociout/src/components/common/header.js
@@ -1,19 +1,59 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import styled from "@emotion/styled";
+import { AppBar, Avatar, InputBase, Toolbar, Typography, Menu, MenuItem } from "@mui/material";
+import React, { useState }  from "react";
 
-const Header = (props) => {
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between"
+})
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  width: "40%",
+}));
+
+const UserBox = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+}));
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <Navbar bg="light" variant="light">
-      <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <AppBar sx={{ backgroundColor: "white"}} position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ color: "black"}}>MeetMax</Typography>
+        <Search><InputBase placeholder="Search for something here..."></InputBase></Search>
+        <UserBox >
+          <Typography variant="span" sx={{ color: "black"}}>Saleh Ahmed</Typography>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            onClick={(e) => setOpen(true)}>
+
+          </Avatar>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
   );
 };
 
