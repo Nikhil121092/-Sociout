fcdc2d92d34e8d8b4c6ba688e33b26f81cda0737
Vaibhav Vishap <vaibhavvishap.javaventures@gmail.com>
2022-09-30 11:45:18 -0400

    Suggested Changes done

--- sociout/src/components/common/header.js
+++ sociout/src/components/common/header.js
@@ -1,17 +1,12 @@
 import styled from "@emotion/styled";
 import { AppBar, Avatar, InputBase, Toolbar, Typography, Menu, MenuItem } from "@mui/material";
-import React, { useState }  from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
-  justifyContent: "space-between"
-})
-
-const Search = styled("div")(({ theme }) => ({
-  backgroundColor: "white",
-  padding: "0 10px",
-  width: "40%",
-}));
+  justifyContent: "space-between",
+});
 
 const UserBox = styled("div")(({ theme }) => ({
   display: "flex",
@@ -22,12 +17,11 @@ const UserBox = styled("div")(({ theme }) => ({
 const Header = () => {
   const [open, setOpen] = useState(false);
   return (
-    <AppBar sx={{ backgroundColor: "white"}} position="sticky">
+    <AppBar sx={{ backgroundColor: "white" }} position="sticky">
       <StyledToolbar>
-        <Typography variant="h6" sx={{ color: "black"}}>MeetMax</Typography>
-        <Search><InputBase placeholder="Search for something here..."></InputBase></Search>
+        <Typography variant="h6" sx={{ color: "black" }}>Sociout</Typography>
         <UserBox >
-          <Typography variant="span" sx={{ color: "black"}}>Saleh Ahmed</Typography>
+          <Typography variant="span" sx={{ color: "black" }}>Saleh Ahmed</Typography>
           <Avatar
             sx={{ width: 30, height: 30 }}
             src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
@@ -50,8 +44,12 @@ const Header = () => {
           horizontal: "right",
         }}
       >
-        <MenuItem>Profile</MenuItem>
-        <MenuItem>Logout</MenuItem>
+        <MenuItem>
+          <Link to="/profile">Profile</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/logout" >Logout</Link>
+        </MenuItem>
       </Menu>
     </AppBar>
   );
